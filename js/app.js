76870237763e1ca3e97bf5dc6162b84cef1c5b4f
Ryan Payne <rpayne@cmithun.com>
2014-07-21 10:35:00 -0500

    Setup

---
+++ js/app.js
@@ -0,0 +1,40 @@
+// Foundation JavaScript
+// Documentation can be found at: http://foundation.zurb.com/docs
+$(document).foundation();
+
+
+//Show and Hide Search Container
+$("#icon-search").click(function(){
+	$(".search").fadeToggle( "slow", "linear" );
+});
+
+//Add to cart CTA
+var CTA_AddToCart = $(".cta-add-to-cart");
+var AddedToCartDialog = $(".added-cart");
+
+
+CTA_AddToCart.click(function(){
+	$(this).toggleClass("added");
+
+	if ($(this).hasClass("added")) {
+		AddedToCartDialog.fadeIn( "slow", "linear" ).delay(500).fadeOut("fast","linear");
+	}
+	else {
+		AddedToCartDialog.hide();
+	}
+});
+
+
+
+
+
+
+//Switching active for the language flags
+$('.language li').click(function(e) {
+    $('.language li.activeItem').removeClass('activeItem');
+    var $this = $(this);
+    if ($this.hasClass('activeItem')) {
+        $this.addClass('activeItem');
+    }
+    e.preventDefault();
+});
